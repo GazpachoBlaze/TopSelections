a45f5d7176a959b315ca82ba5e2f627f5e226b8d
Gazpacho <Gazpachoblaze@gmail.com>
2023-05-04 17:05:59 -0500

    started flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
 
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, MatGridListModule],
   exports: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
