0f41780d307d96db453b5bd6c91eeb488f9adaee
Gazpacho <Gazpachoblaze@gmail.com>
2024-03-21 16:14:56 -0500

    updated database multiplayer

--- src/app/components/multiplayergamespage/multiplayergamespage.component.ts
+++ src/app/components/multiplayergamespage/multiplayergamespage.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-multiplayergamespage',
   templateUrl: './multiplayergamespage.component.html',
   styleUrls: ['./multiplayergamespage.component.css']
 })
-export class MultiplayergamespageComponent {
+export class MultiplayergamespageComponent implements OnInit{
+  title = "hello";
+  name = "chicken";
+  describe = "words";
+  image = "picture";
+  gameItems: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('/assets/games.json').subscribe((data: Object) => {
+      this.gameItems = data as any[];
+    });
+  }
 }
